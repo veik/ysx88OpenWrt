742cfab731f72c341fc244af0cb10e42e2a4aca0
TIAmo <130211313+ysx88@users.noreply.github.com>
2023-08-19 21:07:54 +0800

    Add files via upload

---
+++ armv8/diy-part1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+# Add a feed source
+# sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+# sed -i '$a src-git PWpackages https://github.com/xiaorouji/openwrt-passwall.git;packages' feeds.conf.default
