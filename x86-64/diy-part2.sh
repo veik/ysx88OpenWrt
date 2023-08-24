1dca1ef667efbbd33e33dc4a4cbe05c29518b2d3
TIAmo <130211313+ysx88@users.noreply.github.com>
2023-08-24 14:28:42 +0800

    Update diy-part2.sh

--- x86-64/diy-part2.sh
+++ x86-64/diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.71.3/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.3/g' package/base-files/files/bin/config_generate
 
 # 替换默认内核
 #sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.15/g' target/linux/x86/Makefile
